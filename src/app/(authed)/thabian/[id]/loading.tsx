97dfc6d83a18d276ab320071b21e75ca36037ad7
3raphat <96657413+3raphat@users.noreply.github.com>
2025-02-28 02:06:25 +0700

    feat: add loading components for thabian and wichakan pages

---
+++ src/app/(authed)/thabian/[id]/loading.tsx
@@ -0,0 +1,37 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div className="p-6">
+      <Skeleton className="h-10 w-72 mx-auto mb-4" />
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="w-full h-full border rounded-lg"
+      >
+        <ResizablePanel defaultSize={60}>
+          <div className="flex flex-col gap-4 p-6">
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-32 w-full" />
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-32 w-full" />
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-32 w-full" />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={40}>
+          <div className="flex flex-col gap-4 p-6">
+            <Skeleton className="h-8 w-3/4" />
+            <Skeleton className="h-24 w-full" />
+            <Skeleton className="h-24 w-full" />
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+}
