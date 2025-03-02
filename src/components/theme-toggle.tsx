b0bf124c34941d46987992b660ecf781e94acf53
beambeambeam <supawitmarayat@gmail.com>
2025-03-02 22:04:14 +0700

    feat: new button with effect cause why not

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -18,7 +18,7 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" effect="shineHover">
           <Sun className="reading:-rotate-90 reading:scale-0 h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <BookOpenText className="reading:rotate-0 reading:scale-100 absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all" />
